6c95bc79b931e769df8a15df19d8f9656b171f72
Josanna <josanna@live.se>
2022-10-30 23:12:09 +0100

    Created validator class and named methods

---
+++ BusinessLogic/Validator.cs
@@ -0,0 +1,47 @@
+﻿
+
+namespace BusinessLogic
+{
+    public static class Validator
+    {
+        public static bool HasValue(string text)
+        {
+            bool result = true;
+
+            if (string.IsNullOrEmpty(text))
+            {
+                result = false;
+            }
+            return result;
+        }
+
+        public static bool IsValidUrl(string url) // check if it can be used to retrieve data
+        { 
+            bool result = false;
+
+            return result;
+        }
+
+        public static bool IsDuplicateUrl(string url)
+        {
+            bool result = false;
+
+            return result;
+        }
+
+        public static bool IsDuplicateCategoryName(string name)
+        {
+            bool result = false;
+
+            return result;
+        }
+
+        public static bool IsDuplicateFeedName(string name)
+        {
+            bool result = false;
+
+            return result;
+        }
+
+    }
+}
